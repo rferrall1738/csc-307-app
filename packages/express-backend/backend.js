70f33d3e6d5aa989619b014ce081bdb21c02737f
rferrall1738 <rferrall@calpoly.edu>
2024-10-02 10:48:19 -0700

    step 7 complete can delete people from id and look up people by name and job

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -3,6 +3,7 @@ import express from "express";
 
 const app = express();
 const port = 8000;
+app.use(express.json());
 
 const users = {
   users_list: [
@@ -42,18 +43,40 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
+const findUserByNameAndJob = (name, job) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name && user["job"] ===job
+  );
+};
+
 
 const addUser = (user) => {
   users["users_list"].push(user);
   return user;
 };
 
-const deleteUserByID = (id) =>
-  
+const deleteUserByID = (id) =>{
+  const userIndex =users["users_list"].findIndex((user) => user["id"] === id);
+  if (userIndex !== -1){
+    users["users_list"].splice(userIndex,1);
+    return true;
+  } else{
+    return false;
+  }
+  };
+
+
 
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
+  const job = req.query.job;
+  if (name && job) {
+    let result = findUserByNameAndJob(name,job);
+    result = {users_list: result};
+    res.send(result);
+  }
+
   if (name != undefined) {
     let result = findUserByName(name);
     result = { users_list: result };
@@ -80,7 +103,16 @@ app.post("/users", (req, res) => {
   res.send();
 });
 
-
+app.delete("/users/:id", (req,res) => {
+  const id = req.params["id"];
+  const isDeleted = deleteUserByID(id);
+  if (isDeleted) {
+    res.status(200).send("The User with that ID has been deleted from the database.");
+  } else{
+    res.status(404).send("User is not in the database. Try a valid ID.")
+  }
+  
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
