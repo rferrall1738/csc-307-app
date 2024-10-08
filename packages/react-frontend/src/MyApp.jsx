42fad516f0c29525e75a6ecc2c968007ed8056ba
rferrall1738 <rferrall@calpoly.edu>
2024-10-08 14:40:39 -0700

    frontend shows ID, can make new users that show up on the backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,17 +1,66 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
+
   const [characters, setCharacters] = useState([]);
 
-  function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => i !== index);
-    setCharacters(updated);
+  function fetchUsers(){
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
   }
 
   function updateList(person) {
-    setCharacters([...characters, person]);
+    postUser(person)
+      .then((res) => {
+        if (res.status === 201){
+          return res.json();
+    }     else{
+          throw new Error("Did not create user");
+    }
+  })
+      .then((person) => {
+        setCharacters([...characters, person]);
+  })
+      .catch((error) => {
+        console.log(error);
+    });
+  }
+
+
+  function postUser(person){
+    const promise = fetch("http://localhost:8000/users",{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json", 
+      },
+      body: JSON.stringify(person),
+    });
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+          .then((res) => res.json())
+          .then((json) => setCharacters(json["users_list"]))
+          .catch((error) => { console.log(error); })
+  }, [] );
+
+  function removeOneCharacter(id) {
+    fetch(`http://localhost:8000/users/${id}`,{method: "DELETE"})
+    .then((res) => {
+      if(res.status == 204){
+        setCharacters(characters.filter((character)=> character.id !== id));
+      } else{
+        throw new Error("Did not delete User");
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+
+    
   }
 
   return (
--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -4,6 +4,7 @@ function TableHeader() {
   return (
     <thead>
       <tr>
+        <th>ID</th>
         <th>Name</th>
         <th>Job</th>
         <th>Remove</th>
@@ -14,11 +15,12 @@ function TableHeader() {
 
 function TableBody(props) {
   const rows = props.characterData.map((row, index) => (
-    <tr key={index}>
+    <tr key={row.id}>
+      <td>{row.id}</td>
       <td>{row.name}</td>
       <td>{row.job}</td>
       <td>
-        <button onClick={() => props.removeCharacter(index)}>
+        <button onClick={() => props.removeCharacter(row.id)}>
           Delete
         </button>
       </td>
